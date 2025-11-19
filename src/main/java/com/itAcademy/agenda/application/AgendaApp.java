66b725862aa0b1af35d882940240c25e67c1e017
¨Mirexan¨ <mmregada@gmail.com>
2025-11-19 21:24:29 +0100

    feat: Main menu selector and display + feat: ConsoleInputUtils to readStrings and integers

--- src/main/java/com/itAcademy/agenda/application/AgendaApp.java
+++ src/main/java/com/itAcademy/agenda/application/AgendaApp.java
@@ -1,7 +1,10 @@
 package com.itAcademy.agenda.application;
 
+import com.itAcademy.agenda.application.menu.AppMainMenu;
+
 public class AgendaApp {
 	public static void main(String[] args) {
-
+	AppMainMenu appMenu = new AppMainMenu();
+	appMenu.menuSelector();
 	}
 }
